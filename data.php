c46744485201ec7038e6958e878d568f2cc68976
PinoGabriel <pinogabriel01@gmail.com>
2024-01-15 16:04:03 +0100

    feat: added data.php

---
+++ data.php
@@ -0,0 +1,26 @@
+<?php include __DIR__ . '/Models/Products.php';
+
+$dogFood = [
+    new Products(
+        "Akita Crock",
+        "Cani",
+        "Cibo",
+        "21,99€"
+    ),
+
+    new Products(
+        "Pitbull Gum",
+        "Cani",
+        "Cibo",
+        "43,99€"
+    ),
+
+    new Products(
+        "Golden steak",
+        "Cani",
+        "Cibo",
+        "11,99€"
+    ),
+];
+
+
--- index.php
+++ index.php
@@ -1,31 +1,5 @@
 <?php
-include __DIR__ . '/Models/Products.php';
-
-
-$dogFood = [
-    new Products(
-        "Akita Crock",
-        "Cani",
-        "Cibo",
-        "21,99€"
-    ),
-
-    new Products(
-        "Pitbull Gum",
-        "Cani",
-        "Cibo",
-        "43,99€"
-    ),
-
-    new Products(
-        "Golden steak",
-        "Cani",
-        "Cibo",
-        "11,99€"
-    ),
-];
-
-
+include __DIR__ . '/data.php';
 ?>
 
 <!DOCTYPE html>
